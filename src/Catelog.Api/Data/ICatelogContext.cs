be8d38a9eafde34faa1437ae6dcfe1420b63530a
Hebatul Wadud <hebatul.wadud@brainstation23.com>
2024-02-21 17:19:32 +0600

    catelog api added

---
+++ src/Catelog.Api/Data/ICatelogContext.cs
@@ -0,0 +1,10 @@
+﻿using Catelog.Api.Entities;
+using MongoDB.Driver;
+
+namespace Catelog.Api.Data
+{
+    public interface ICatelogContext
+    {
+        IMongoCollection<Product> Products { get; }
+    }
+}
---
+++ src/Catelog.Api/Program.cs
@@ -0,0 +1,36 @@
+using Catelog.Api.Data;
+using Catelog.Api.Repository;
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Catelog.Api", Version = "v1" });
+});
+
+builder.Services.AddScoped<ICatelogContext, CatelogContext>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Catelog.Api v1"));
+}
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
